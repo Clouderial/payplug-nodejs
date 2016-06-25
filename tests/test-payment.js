beb1734b1d6fea2bf07d296a706899aa5cf257bc
Jean-Marc Collin <jm.collin.78@gmail.com>
2016-06-25 12:49:45 +0200

    Add Payment.list

--- tests/test-payment.js
+++ tests/test-payment.js
@@ -14,67 +14,99 @@
  */
 var expect = require('chai').expect; // jshint ignore:line
 var log = require('log4js').getLogger('testu'),
-    jmcnet = require('jmcnet'),
-    jmcnetException = jmcnet.exception,
-    //    _ = require('lodash'),
-    ppnj = require('lib/payplug-nodejs.js'),
-    PayPlugAPI = ppnj.PayPlugAPI,
-    Payment = require('lib/payment.js').Payment,
-    config = require('tests/config.json');
+	jmcnet = require('jmcnet'),
+	jmcnetException = jmcnet.exception,
+	//    _ = require('lodash'),
+	ppnj = require('lib/payplug-nodejs.js'),
+	PayPlugAPI = ppnj.PayPlugAPI,
+	Payment = require('lib/payment.js').Payment,
+	config = require('tests/config.json');
 
 // The tests
 describe.only('<PlayPlug Paymnt API Unit Test>', function () {
-    var payplugapi;
-    before(function () {
-        log.debug('--> Testing "Payment.beforeAll"');
-        payplugapi = new PayPlugAPI(config.testSecretKey);
-        log.debug('<-- EndOf "Payment.beforeAll"');
-    });
+	var payplugapi;
+	before(function () {
+		log.debug('--> Testing "Payment.beforeAll"');
+		payplugapi = new PayPlugAPI(config.testSecretKey, {
+			sucessReturnUrl: 'https://example.net/payplugapi/test/success?tracker=',
+        	cancelReturnUrl: 'https://example.net/payplugapi/test/cancel?tracker=',
+        	notificationUrl: 'https://example.net/payplugapi/test/notifications?tracker=',
+		});
+		log.debug('<-- EndOf "Payment.beforeAll"');
+	});
 
-    it('Should be not possible to instanciate Payment without parameter', function (done) {
-        try {
-            new Payment();
-            done('We should not be there');
-        } catch (err) {
-            expect(err).to.be.instanceof(jmcnetException.TechnicalException);
-            done();
-        }
-    });
+	it('Should be not possible to instanciate Payment without parameter', function (done) {
+		try {
+			new Payment();
+			done('We should not be there');
+		} catch (err) {
+			expect(err).to.be.instanceof(jmcnetException.TechnicalException);
+			done();
+		}
+	});
 
-    it('Should be not possible to instanciate Payment with PayPlugAPI not authenticated', function (done) {
-        try {
-            new Payment(payplugapi, 'paymentTracker', {});
-            done('We should not be there');
-        } catch (err) {
-            expect(err).to.be.instanceof(jmcnetException.FunctionalException);
-            done();
-        }
-    });
+	it('Should be not possible to instanciate Payment with PayPlugAPI not authenticated', function (done) {
+		try {
+			new Payment(payplugapi, 'paymentTracker', {});
+			done('We should not be there');
+		} catch (err) {
+			expect(err).to.be.instanceof(jmcnetException.FunctionalException);
+			done();
+		}
+	});
 
-    it('Should be possible to authenticate', function (done) {
-        log.debug('--> Start "Should be possible to authenticate"');
-        payplugapi.authenticated = true;
-        done();
-        // TODO decomment this and remove above
-/*
-        payplugapi.authenticate()
-            .then(function (result) {
-                expect(payplugapi.authenticated).to.be.true;
-                log.debug('<-- EndOf "Should be possible to authenticate"');
-                done();
-            })
-            .fail(done)
-            .done();
-*/
-    });
+	it('Should be possible to authenticate', function (done) {
+		log.debug('--> Start "Should be possible to authenticate"');
+		payplugapi.authenticate()
+			.then(function (result) {
+				expect(payplugapi.authenticated).to.be.true;
+				log.debug('<-- EndOf "Should be possible to authenticate"');
+				done();
+			})
+			.fail(done)
+			.done();
+	});
 
-    it('Should be not possible to instanciate Payment without paymentTracker', function (done) {
-        try {
-            new Payment(payplugapi);
-            done('We should not be there');
-        } catch (err) {
-            expect(err).to.be.instanceof(jmcnetException.TechnicalException);
-            done();
-        }
-    });
+	it('Should be not possible to instanciate Payment without paymentTracker', function (done) {
+		try {
+			new Payment(payplugapi);
+			done('We should not be there');
+		} catch (err) {
+			expect(err).to.be.instanceof(jmcnetException.TechnicalException);
+			done();
+		}
+	});
+
+	var payment;
+	it('Should be possible to instanciate a Payment', function (done) {
+		payment = new Payment(payplugapi, 'paymentid', {
+			'amount': 1000,
+			'currency': 'EUR',
+			'customer': {
+				'email': 'testu@payplug-nodejs.com',
+				'first_name': 'Testu',
+				'last_name': 'Payplug-nodejs'
+			},
+			'save_card': 'false',
+			'force_3ds': true
+		});
+		payment.sendCreate()
+			.then(function (p) {
+				expect(p.isSend).to.be.true;
+				expect(p.payment.metadata.paymentTracker);
+				done();
+			})
+			.fail(done)
+			.done();
+	});
+	
+	it('Should be possible to list the payments', function(done){
+		Payment.list(payplugapi)
+		.then(function(list) {
+			expect(list.length).to.be.above(0);
+			done();
+		})
+		.fail(done)
+		.done();
+	});
 });
